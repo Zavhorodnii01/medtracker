bb6b0bbfc9a2ed266514875230146e21ca66b0d6
Nazarii <167976288+Zavhorodnii01@users.noreply.github.com>
2026-01-12 16:40:19 +0100

    Revert "Part1"

--- medtrackerapp/urls.py
+++ medtrackerapp/urls.py
@@ -1,20 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MedicationViewSet, DoseLogViewSet, NoteViewSet
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework.permissions import AllowAny
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Software engineering lab",
-        default_version="v1",
-        description="API documentation for the lab",
-    ),
-    public=True,
-    permission_classes=(AllowAny,),
-    authentication_classes=[],
-)
 
 router = DefaultRouter()
 router.register("medications", MedicationViewSet, basename="medication")
@@ -23,6 +10,4 @@ router.register("notes", NoteViewSet, basename="note")
 
 urlpatterns = [
     path("", include(router.urls)),
-    # Add this line below:
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
